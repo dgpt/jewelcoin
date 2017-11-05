13d3dffc1f0b3fabe414a198b6efcde3f913b123
dgpt <dogpantz@gmail.com>
2017-11-04 20:32:52 -0600

    JewelCoin configuration and initial refactoring
    Sets initial configuration data for jewelcoin
    
    also eplaces monerod with jewelcoind and updates
    services and config files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu:16.04
 
-ENV SRC_DIR /usr/local/src/monero
+ENV SRC_DIR /usr/local/src/jewelcoin
 
 RUN set -x \
   && buildDeps=' \
@@ -16,7 +16,7 @@ RUN set -x \
   && apt-get -qq update \
   && apt-get -qq --no-install-recommends install $buildDeps
 
-RUN git clone https://github.com/monero-project/monero.git $SRC_DIR
+RUN git clone https://github.com/dgpt/jewelcoin-monero $SRC_DIR
 WORKDIR $SRC_DIR
 RUN make -j$(nproc) release-static
 
@@ -26,7 +26,7 @@ RUN cp build/release/bin/* /usr/local/bin/ \
   && apt-get -qq --auto-remove purge $buildDeps
 
 # Contains the blockchain
-VOLUME /root/.bitmonero
+VOLUME /root/.jewelchain
 
 # Generate your wallet via accessing the container and run:
 # cd /wallet
@@ -35,11 +35,11 @@ VOLUME /wallet
 
 ENV LOG_LEVEL 0
 ENV P2P_BIND_IP 0.0.0.0
-ENV P2P_BIND_PORT 18080
+ENV P2P_BIND_PORT 18880
 ENV RPC_BIND_IP 127.0.0.1
-ENV RPC_BIND_PORT 18081
+ENV RPC_BIND_PORT 18881
 
-EXPOSE 18080
-EXPOSE 18081
+EXPOSE 18880
+EXPOSE 18881
 
-CMD monerod --log-level=$LOG_LEVEL --p2p-bind-ip=$P2P_BIND_IP --p2p-bind-port=$P2P_BIND_PORT --rpc-bind-ip=$RPC_BIND_IP --rpc-bind-port=$RPC_BIND_PORT
+CMD jewelcoind --log-level=$LOG_LEVEL --p2p-bind-ip=$P2P_BIND_IP --p2p-bind-port=$P2P_BIND_PORT --rpc-bind-ip=$RPC_BIND_IP --rpc-bind-port=$RPC_BIND_PORT
--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -122,7 +122,7 @@
 
 #define ALLOW_DEBUG_COMMANDS
 
-#define CRYPTONOTE_NAME                         "bitmonero"
+#define CRYPTONOTE_NAME                         "jewelcoin"
 #define CRYPTONOTE_POOLDATA_FILENAME            "poolstate.bin"
 #define CRYPTONOTE_BLOCKCHAINDATA_FILENAME      "data.mdb"
 #define CRYPTONOTE_BLOCKCHAINDATA_LOCK_FILENAME "lock.mdb"
@@ -146,28 +146,28 @@ namespace config
   uint64_t const BASE_REWARD_CLAMP_THRESHOLD = ((uint64_t)100000000); // pow(10, 8)
   std::string const P2P_REMOTE_DEBUG_TRUSTED_PUB_KEY = "0000000000000000000000000000000000000000000000000000000000000000";
 
-  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 18;
-  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 19;
-  uint16_t const P2P_DEFAULT_PORT = 18080;
-  uint16_t const RPC_DEFAULT_PORT = 18081;
-  uint16_t const ZMQ_RPC_DEFAULT_PORT = 18082;
+  uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 81;
+  uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 82;
+  uint16_t const P2P_DEFAULT_PORT = 18880;
+  uint16_t const RPC_DEFAULT_PORT = 18881;
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = 18882;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0xF5 ,0x03, 0xA1, 0x51 , 0x21, 0x06 , 0xB4, 0x91, 0x69, 0x42, 0x0A, 0xC5, 0x22, 0xB0, 0x87, 0x35
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+  std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210102851bb81292f2ec2eef3a88e9a229a3115817c044b585445f0c22050ebd3a32";
   uint32_t const GENESIS_NONCE = 10000;
 
   namespace testnet
   {
-    uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 53;
-    uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
-    uint16_t const P2P_DEFAULT_PORT = 28080;
-    uint16_t const RPC_DEFAULT_PORT = 28081;
-    uint16_t const ZMQ_RPC_DEFAULT_PORT = 28082;
+    uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 81;
+    uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 82;
+    uint16_t const P2P_DEFAULT_PORT = 28880;
+    uint16_t const RPC_DEFAULT_PORT = 28881;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 28882;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0xF5 ,0x03, 0xA1, 0x51 , 0x21, 0x06 , 0xB4, 0x91, 0x69, 0x42, 0x0A, 0xC5, 0x22, 0xB0, 0x87, 0x35
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+    std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101ef5188013a12f188df857c4dddfccf2b72b07c37b505c043e3aaa4d8679a459c";
     uint32_t const GENESIS_NONCE = 10001;
   }
 }
